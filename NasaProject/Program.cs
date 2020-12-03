8e7e337470cd7f17b634c392ba6cd16ccee6a6fb
Hugo Carvalho <huguinho66@live.com>
2020-12-03 14:49:27 +0000

    first commit

---
+++ NasaProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NasaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
